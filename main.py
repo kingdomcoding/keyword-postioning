2aea65db03f7ca2034f07b82c373c63d18a79722
Kingdom Coder <kingdomcoding@gmail.com>
2021-04-24 10:27:49 +0100

    Cohorts written to file

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 import itertools
 import re
 
-def separate_authors_from_titles(articles):
-    titles = []
-
-    for article in articles:
-        tokens = article.split("\n")
-        title = tokens[0]
-        titles.append(title)
-
-    
-    return titles
-
-def get_titles(filename):
+def get_articles(filename):
     articles = []
     with open(filename, encoding="utf8") as file:
         article = ""
@@ -27,7 +16,17 @@ def get_titles(filename):
         if article:
             articles.append(article)
 
-    titles = separate_authors_from_titles(articles)
+    return articles
+
+def separate_authors_from_titles(articles):
+    titles = []
+
+    for article in articles:
+        tokens = article.split("\n")
+        title = tokens[0]
+        titles.append(title)
+
+    
     return titles
 
 def normalize_to_lowercase(titles):
@@ -48,11 +47,11 @@ def remove_stop_words(titles):
     cleaned_titles = []
     with open("Search Engine Stop Words.txt", encoding="utf8") as file:
         stop_words = file.read().splitlines()
+        
         for title in titles:
             cleaned_title = " ".join((set(title.split()) - set(stop_words)))
             cleaned_titles.append(cleaned_title)
 
-            # print(f'{title}\n{cleaned_title}\n\n')
     return cleaned_titles    
 
 def clean_titles(titles):
@@ -105,8 +104,6 @@ def generate_combinations_of_words(words_and_sources, minimum_number_of_words_ar
 
 def find_titles_that_share_combination_of_words(words_and_sources):
     list_of_titles = [title for title in words_and_sources.values()]
-    # print(words_and_sources)
-    # print(list_of_titles)
 
     repeated_items_set = set(list_of_titles[0])
 
@@ -137,16 +134,69 @@ def filter_combinations_of_words_with_enough_articles_to_form_a_cohort(combinati
     
     return valid_cohorts
 
+def combine_repeated_groups_of_articles_into_cohorts(groups_of_articles):
+    flipped = {}
+  
+    for key, value in groups_of_articles.items():
+        value = tuple(value)
+        key = set(key)
+        if value not in flipped:
+            flipped[value] = key
+        else:
+            flipped[value] = flipped[value] | key
+
+    combined = {}
+    for key, value in flipped.items():
+        value = tuple(value)
+        key = set(key)
+        combined[value] = list(key)
+
+    # print(combined)
+    return combined        
+
 def find_valid_cohorts(words_and_sources, minimum_number_of_words_articles_in_a_cohort_must_share_in_their_title, minimum_number_of_articles_that_must_be_in_a_cohort):
     combinations_of_words = generate_combinations_of_words(words_and_sources, minimum_number_of_words_articles_in_a_cohort_must_share_in_their_title)
     combinations_of_words_with_list_of_article_titles_that_share_them = find_titles_that_share_combinations_of_words(words_and_sources, combinations_of_words)
     
-    valid_cohorts = filter_combinations_of_words_with_enough_articles_to_form_a_cohort(combinations_of_words_with_list_of_article_titles_that_share_them, minimum_number_of_articles_that_must_be_in_a_cohort)
+    groups_of_articles = filter_combinations_of_words_with_enough_articles_to_form_a_cohort(combinations_of_words_with_list_of_article_titles_that_share_them, minimum_number_of_articles_that_must_be_in_a_cohort)
 
+    valid_cohorts = combine_repeated_groups_of_articles_into_cohorts(groups_of_articles)
     return valid_cohorts
 
+def format_cohorts_with_article_names(cohorts, articles):
+    cohort_with_article_names = {}
+    for cohort in cohorts:
+        article_names = []
+        for article in cohorts[cohort]:
+            article_names.append(articles[article])
+        cohort_with_article_names[cohort] = article_names
+    
+    return cohort_with_article_names
+
+def get_list_of_cohort_articles(cohorts, articles):
+    article_indices = list(set([article_index for sublist in cohorts.values() for article_index in sublist]))
+    article_names = [articles[index] for index in article_indices]
+    return article_names
+
+def write_cohorts_to_file(cohorts_with_article_names):
+    with open("Cohorts.txt", "w", encoding="utf8") as file:
+        for cohort in cohorts_with_article_names:
+            file.write("************************************************************\n")
+            
+            cohort_words = ", ".join(cohort)
+            file.write(f"Cohort Words: {cohort_words}\n\n")
+
+            file.write("Cohort Articles:\n")
+            for article in cohorts_with_article_names[cohort]:
+                file.write(f"{article}\n")
+            
+            file.write("************************************************************\n\n")
+
+
 def main():
-    titles = get_titles("2016 Software Engineering Papers.txt")
+    articles = get_articles("2016 Software Engineering Papers.txt")
+
+    titles = separate_authors_from_titles(articles)
     titles = clean_titles(titles)
 
     words_and_sources = list_words_and_sources(titles)
@@ -158,7 +208,12 @@ def main():
     minimum_number_of_articles_that_must_be_in_a_cohort = 3
     valid_cohorts = find_valid_cohorts(high_frequency_words_and_sources, minimum_number_of_words_articles_in_a_cohort_must_share_in_their_title, minimum_number_of_articles_that_must_be_in_a_cohort)
     
-    print(valid_cohorts)
+    cohorts_with_article_names = format_cohorts_with_article_names(valid_cohorts, articles)
+
+    list_of_cohort_articles = get_list_of_cohort_articles(valid_cohorts, articles)
+    
+    write_cohorts_to_file(cohorts_with_article_names)
+    # write_list_of_files_in_cohorts()
 
 if __name__ == "__main__":
     main()
